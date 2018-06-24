eda3135b741ea52ac68758a3e72e0faffb4a0090
miracuthbert <miracuthbert@gmail.com>
2018-06-24 03:45:23 +0300

    Add Category Filters
    Add filters for 'category' and 'categories' based relations

---
+++ app/Domain/Categories/Filters/CategoryFilter.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cuthbert Mirambo
+ * Date: 6/24/2018
+ * Time: 3:14 AM
+ */
+
+namespace Rims\Domain\Categories\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use Rims\App\Filters\FilterAbstract;
+use Rims\Domain\Categories\Models\Category;
+
+class CategoryFilter extends FilterAbstract
+{
+    /**
+     * Apply filter.
+     *
+     * @param Builder $builder
+     * @param $value
+     *
+     * @return mixed
+     */
+    public function filter(Builder $builder, $value)
+    {
+        if ($value === null) {
+            return $builder;
+        }
+
+        $category = Category::with(['descendants'])
+            ->where('slug', $value)->first();
+
+        $categories = $category->descendants->pluck('id')->prepend($category->id);
+
+        return $builder->whereHas('category', function (Builder $builder) use ($categories) {
+            $builder->whereIn('category_id', $categories);
+        });
+    }
+}

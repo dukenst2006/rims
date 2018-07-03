a2107eec0434454785268cc7e8b0f921c265b434
miracuthbert <miracuthbert@gmail.com>
2018-07-04 02:33:23 +0300

    Update UserEducation model
    Add attributes to return enrollment and graduation years
    Add missing comments

--- app/Domain/Users/Models/UserEducation.php
+++ app/Domain/Users/Models/UserEducation.php
@@ -24,11 +24,52 @@ class UserEducation extends Model
         'ended_at',
     ];
 
+    protected $appends = [
+        'enrollmentYear',
+        'graduationYear'
+    ];
+
+    /**
+     * Get enrollment year from start date.
+     *
+     * @return int|null
+     */
+    public function getEnrollmentYearAttribute()
+    {
+        return $this->started_at == null ? null : Carbon::parse($this->started_at)->year;
+    }
+
+    /**
+     * Get graduation year from end date.
+     *
+     * @return int|null
+     */
+    public function getGraduationYearAttribute()
+    {
+        if ($this->ended_at == null) {
+            return $this->started_at == null ? null : 'Ongoing';
+        } else {
+            return Carbon::parse($this->ended_at)->year;
+        }
+    }
+
+    /**
+     * Get formatted start date.
+     *
+     * @param $value
+     * @return string
+     */
     public function getStartedAtAttribute($value)
     {
         return $value == null ? $value : Carbon::parse($value)->toDateString();
     }
 
+    /**
+     * Get formatted end date.
+     *
+     * @param $value
+     * @return string
+     */
     public function getEndedAtAttribute($value)
     {
         return $value == null ? $value : Carbon::parse($value)->toDateString();
